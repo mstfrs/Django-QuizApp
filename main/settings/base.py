2ab7b2b1aa306ea5811889001ea5190ba20616b2
mstfrs <mstfrs68@gmail.com>
2022-03-10 10:09:06 +0300

    heroku

--- main/settings/base.py
+++ main/settings/base.py
@@ -28,8 +28,10 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
+DEBUG = False 
+ALLOWED_HOSTS = ['mors-quizapp.herokuapp.com', 'localhost', '127.0.0.1'] 
+DISABLE_COLLECTSTATIC = 1
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
